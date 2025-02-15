627bb230771f698342692c126ed6d9c199820749
李明 <liming_longxi@163.com>
2025-02-15 18:33:54 +0800

    【add】新增读取图片和颜色转化的代码

---
+++ openCVStudy/Demo/cvtColor.cs
@@ -0,0 +1,33 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openCVStudy.Demo
+{
+    internal class CvtColor
+    {
+        public CvtColor() {
+            Mat img_rgb = Cv2.ImRead("E:\\program\\openCVStudy\\openCVStudy\\Assets\\01.jpeg", ImreadModes.Color);
+            Mat img_gray = new Mat();
+            Mat img_hsv = new Mat();
+            Mat img_gray2rgb = new Mat();
+            Mat img_hsv2rgb = new Mat();
+            Cv2.CvtColor(img_rgb, img_hsv, ColorConversionCodes.RGB2HSV);
+            Cv2.CvtColor(img_rgb, img_gray, ColorConversionCodes.RGB2GRAY);
+            Cv2.CvtColor(img_gray, img_gray2rgb, ColorConversionCodes.GRAY2RGB);
+            Cv2.CvtColor(img_hsv, img_hsv2rgb, ColorConversionCodes.HSV2BGR);
+            Console.WriteLine("rgb的通道数为" + img_rgb.Channels());
+            Console.WriteLine("hsv的通道数为" + img_hsv.Channels());
+            Console.WriteLine("gray的通道数为" + img_gray.Channels());
+            Cv2.ImShow("rgb", img_rgb);
+            Cv2.ImShow("gray", img_gray);
+            Cv2.ImShow("hsv", img_hsv);
+            Cv2.ImShow("gray2rgb", img_gray2rgb);
+            Cv2.ImShow("hsv2rgb", img_hsv2rgb);
+            Cv2.WaitKey();
+        }
+    }
+}
--- openCVStudy/Program.cs
+++ openCVStudy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using openCVStudy.Demo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,11 @@ namespace openCVStudy
         static void Main(string[] args)
         {
             Console.WriteLine("开始执行程序");
+            // 读取图片
+            // ReadImage readImage =  new ReadImage();
+
+            // 图片颜色转化
+            CvtColor cvtColor = new CvtColor();
             Console.ReadKey();
         }
     }
